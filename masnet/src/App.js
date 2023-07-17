e37b0e5bb03157a48b43c2b86c8ed383312e2d6f
masproo22 <m.abbaszadeh2002@gmail.com>
2023-07-17 12:45:59 +0330

    complete seo-page

--- masnet/src/App.js
+++ masnet/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./navbar/navbar-app"
 import WelcomePage from "./welcom-page/welcome-page-app"
+import SeoPage from './seo-page/seo-page-app';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <WelcomePage></WelcomePage>
+      <SeoPage></SeoPage>
     </div>
   );
 }
