fc374a8e1a2e63a93c4e7fcd5b1d744551c5feea
masproo22 <m.abbaszadeh2002@gmail.com>
2023-07-17 11:27:06 +0330

    complete welcome-page

--- masnet/src/App.js
+++ masnet/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./navbar/navbar-app"
+import WelcomePage from "./welcom-page/welcome-page-app"
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <WelcomePage></WelcomePage>
     </div>
   );
 }
