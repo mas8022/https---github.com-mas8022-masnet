0da911ef69e808b5848c92710d05b45a406f195c
masproo22 <m.abbaszadeh2002@gmail.com>
2023-07-17 10:45:19 +0330

    complete navbar

--- masnet/src/navbar/navbar-app.js
+++ masnet/src/navbar/navbar-app.js
@@ -4,7 +4,18 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-        <nav></nav>
+        <div className="nav">
+            <div className='logo'>MASNET</div>
+            <div className='div-main-ul-nav'>
+                <ul className="main-ul-nav">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>Shop</li></a>
+                    <a href="#"><li>My basket</li></a>
+                    <a href="#"><li>Profile</li></a>
+                    <a href="#"><li>About us</li></a>
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
